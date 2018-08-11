8c21a02596b9e954217490403713a165da3f4eed
CodyTseng <cody880528@hotmail.com>
2018-08-11 13:37:17 +0800

    LAB3 done

--- NCTU-Computer-Organization/LAB3/direct_mapped_cache.cpp
+++ NCTU-Computer-Organization/LAB3/direct_mapped_cache.cpp
@@ -1,62 +1,83 @@
-#include <iostream>
-#include <stdio.h>
 #include <math.h>
+#include <stdio.h>
+#include <iomanip>
+#include <iostream>
 
 using namespace std;
 
-struct cache_content
-{
-	bool v;
-	unsigned int tag;
+struct cache_content {
+    bool v;
+    unsigned int tag;
     // unsigned int	data[16];
 };
 
 const int K = 1024;
 
-double log2(double n)
-{  
-    // log(n) / log(2) is log2.
-    return log(n) / log(double(2));
+inline int log2(int n) {
+    int l = 0;
+    while (n >>= 1) l++;
+    return l;
 }
 
+double simulate(int cache_size, int block_size, string file_name) {
+    unsigned int tag, index, x;
 
-void simulate(int cache_size, int block_size)
-{
-	unsigned int tag, index, x;
+    int offset_bit = (int)log2(block_size);
+    int index_bit = (int)log2(cache_size / block_size);
+    int line = cache_size >> (offset_bit);
 
-	int offset_bit = (int)log2(block_size);
-	int index_bit = (int)log2(cache_size / block_size);
-	int line = cache_size >> (offset_bit);
+    cache_content *cache = new cache_content[line];
 
-	cache_content *cache = new cache_content[line];
-	
-    cout << "cache line: " << line << endl;
+    // cout << "cache line: " << line << endl;
 
-	for(int j = 0; j < line; j++)
-		cache[j].v = false;
-	
-    FILE *fp = fopen("test.txt", "r");  // read file
-	
-	while(fscanf(fp, "%x", &x) != EOF)
-    {
-		cout << hex << x << " ";
-		index = (x >> offset_bit) & (line - 1);
-		tag = x >> (index_bit + offset_bit);
-		if(cache[index].v && cache[index].tag == tag)
-			cache[index].v = true;    // hit
-		else
-        {
-			cache[index].v = true;  // miss
-			cache[index].tag = tag;
-		}
-	}
-	fclose(fp);
+    for (int j = 0; j < line; j++) cache[j].v = false;
 
-	delete [] cache;
+    FILE *fp = fopen(file_name.c_str(), "r");  // read file
+
+    int cnt = 0, miss = 0;
+    while (fscanf(fp, "%x", &x) != EOF) {
+        cnt++;
+        // cout << hex << x << " ";
+        index = (x >> offset_bit) & (line - 1);
+        tag = x >> (index_bit + offset_bit);
+        if (cache[index].v && cache[index].tag == tag) {
+            cache[index].v = true;  // hit
+        } else {
+            miss++;
+            cache[index].v = true;  // miss
+            cache[index].tag = tag;
+        }
+    }
+    fclose(fp);
+
+    delete[] cache;
+    return (double)miss / cnt * 100;
 }
-	
-int main()
-{
-	// Let us simulate 4KB cache with 16B blocks
-	simulate(4 * K, 16);
+
+int main() {
+    // Let us simulate 4KB cache with 16B blocks
+    string files[] = {"DCACHE.txt", "ICACHE.txt"};
+    for (auto file_name : files) {
+        cout << "File name:" << file_name << endl;
+        cout << '\t';
+
+        for (int block_size = 4; block_size <= 128; block_size <<= 1) {
+            cout << block_size << '\t';
+        }
+        cout << "(<- block size)" << endl;
+
+        for (int cache_size = 32; cache_size <= 256; cache_size <<= 1) {
+            cout << cache_size << '\t';
+            for (int block_size = 4; block_size <= 128; block_size <<= 1) {
+                if (block_size > cache_size) {
+                    cout << "N/A\t";
+                    continue;
+                }
+                cout << fixed << setprecision(4)
+                     << simulate(cache_size, block_size, file_name) << '\t';
+            }
+            cout << endl;
+        }
+        cout << endl;
+    }
 }
---
+++ NCTU-Computer-Organization/LAB3/direct_mapped_cache_lru.cpp
@@ -0,0 +1,91 @@
+#include <algorithm>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct cache_content {
+    cache_content() : v(false), vis(0) {}
+    bool v;
+    int vis;
+    uint32_t tag;
+};
+
+inline int log2(int n) {
+    int l = 0;
+    while (n >>= 1) l++;
+    return l;
+}
+
+double simulate(int cache_size, int assoc, string file_name) {
+    int offset_bit = 6;
+    int assoc_bit = log2(assoc);
+    int line = cache_size >> assoc_bit >> offset_bit;
+    int index_bit = log2(cache_size) - offset_bit - assoc_bit;
+
+    vector<vector<cache_content>> cache(line, vector<cache_content>(assoc));
+
+    fstream file;
+    file.open(file_name, ios::in);
+
+    uint32_t addr, idx, tag;
+    int cnt = 0, miss = 0;
+    while (file >> hex >> addr) {
+        cnt++;
+        idx = (addr >> offset_bit) & (line - 1);
+        tag = addr >> (index_bit + offset_bit);
+
+        auto it = find_if(
+            cache[idx].begin(), cache[idx].end(),
+            [&tag](cache_content x) { return x.v == true && x.tag == tag; });
+
+        if (it != cache[idx].end()) {
+            for (auto &i : cache[idx]) {
+                if (i.vis < it->vis) i.vis++;
+            }
+            it->vis = 0;
+        } else {
+            miss++;
+            int max = -1;
+            vector<cache_content>::iterator max_it;
+            for (auto i = cache[idx].begin(); i != cache[idx].end(); i++) {
+                if (!i->v) {
+                    max_it = i;
+                    break;
+                }
+                if (i->vis > max) max_it = i, max = i->vis;
+                i->vis++;
+            }
+
+            max_it->vis = 0;
+            max_it->v = true;
+            max_it->tag = tag;
+        }
+    }
+    return static_cast<double>(miss) / cnt * 100;
+}
+
+int main(int argc, char const *argv[]) {
+    string files[] = {"LU.txt", "RADIX.txt"};
+    for (auto file_name : files) {
+        cout << "File name:" << file_name << endl;
+        cout << '\t';
+
+        for (int assoc = 1; assoc <= 8; assoc <<= 1) {
+            cout << assoc << '\t';
+        }
+        cout << "(<- associativity)" << endl;
+
+        for (int cache_size = 1; cache_size <= 32; cache_size <<= 1) {
+            cout << cache_size << "K\t";
+            for (int assoc = 1; assoc <= 8; assoc <<= 1) {
+                cout << fixed << setprecision(4)
+                     << simulate(cache_size * 1024, assoc, file_name) << '\t';
+            }
+            cout << endl;
+        }
+        cout << endl;
+    }
+    return 0;
+}
