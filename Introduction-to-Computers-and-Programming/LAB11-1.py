12f9c3c54dc8572a1d4cabc085b1bbd9d4b6ac7d
CodyTseng <cody880528@hotmail.com>
2019-04-02 18:06:04 +0800

    Rename

--- Introduction-to-Computers-and-Programming/LAB11-1.py
+++
@@ -1,2 +0,0 @@
-s = "X-DSPAM-Confidence: 0.8475"
-print(float(s[s.find(":")+1:]))
--- Introduction-to-Computers-and-Programming/hw8-1_0611262.py
+++
@@ -1,8 +0,0 @@
-from statistics import mean
-# Read all lines from file
-# filter out line that starts with X-DSPAM-Confidence
-# convert to float and calc mean
-print("Average spam confidence: {}".
-      format(mean([float(i[i.find(":")+1:])
-                   for i in open(input("Enter the file name: ")).readlines()
-                   if i.startswith("X-DSPAM-Confidence:")])))
